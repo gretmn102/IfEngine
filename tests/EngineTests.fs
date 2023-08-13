b5d6cb400317c9884d44d38706540148af62e301
gretmn102 <erlgreywatson@gmail.com>
2023-08-13 12:28:59 +0300

    test: refact: move `EngineTests.Utils` to `Tests.Engine.Utils`

--- tests/EngineTests.fs
+++ tests/EngineTests.fs
@@ -9,38 +9,7 @@ open IfEngine.SyntaxTree.Helpers
 open IfEngine.Engine
 
 open Tests.SyntaxTree.Helpers
-
-// todo: replace `Utils`
-module Utils =
-    let equalPrint exp (cmd: OutputMsg<'Content, 'CustomStatement>) =
-        match cmd with
-        | OutputMsg.Print(text) ->
-            Assert.Equal("", exp, text)
-        | x ->
-            failwithf "expected Print %A but:\n%A" exp x
-
-    let equalMenu exp (cmd: OutputMsg<'Content, 'CustomStatement>) =
-        match cmd with
-        | OutputMsg.Choices(text, selects) ->
-            Assert.Equal("", exp, (text, selects))
-        | x ->
-            failwithf "expected Menu %A\nbut:\n%A" exp x
-
-    let equalEnd (cmd: OutputMsg<'Content, 'CustomStatement>) =
-        match cmd with
-        | OutputMsg.End -> ()
-        | x ->
-            failwithf "expected End but:\n%A" x
-
-    let equalCustomStatement (exp: 'CustomStatement) eq (cmd: OutputMsg<'Content, 'CustomStatement>) =
-        match cmd with
-        | OutputMsg.CustomStatement(customStatement) ->
-            if not <| eq exp customStatement then
-                failwithf "expected: %A\nactual: %A" exp customStatement
-        | x ->
-            failwithf "expected Menu %A\nbut:\n%A" exp x
-
-open Utils
+open Tests.Engine.Utils
 
 let applesCount = VarsContainer.createNum "apples"
 let isApplesOnRightRoad = VarsContainer.createBool "isApplesOnRightRoad"
